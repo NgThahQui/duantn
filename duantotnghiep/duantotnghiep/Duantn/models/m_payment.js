d5768314d4c70d0c7b9bc87518281908578d82dc
Nguyễn Thanh Qui <quintps39335@gmail.com>
2025-05-21 20:23:34 +0700

    Delete duantotnghiep directory

--- duantotnghiep/duantotnghiep/Duantn/models/m_payment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose');
-const Schema = mongoose.Schema;
-
-const paymentSchema = new Schema({
-    id_order: { type: Schema.Types.ObjectId, ref: "Order", required: true },
-    name: { type: String, required: true }
-});
-
-module.exports = mongoose.models.Payment || mongoose.model("Payment", paymentSchema);
